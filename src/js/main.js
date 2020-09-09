a20e77c900f0ab1c79afa8ca40f71a8399f17056
AleksKirienko <aleks.Kirienko@bk.ru>
2020-09-09 18:04:37 +0700

    Add others cars on the road

--- src/js/main.js
+++ src/js/main.js
@@ -20,18 +20,33 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
 };
+
+function getQuantityElements(heightElement) {
+    return document.documentElement.clientHeight / heightElement + 1;
+}
 
 function startGame() {
     start.classList.add('hide');
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
         line.style.top = (i * 80) + 'px';
         line.y = i * 80;
         gameArea.appendChild(line);
+    }
+
+    for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url(./assets/cars/car2.png) center / cover no-repeat';
+        gameArea.appendChild(enemy);
     }
 
     setting.start = true;
@@ -45,6 +60,7 @@ function playGame() {
     console.log('Play game!');
     if (setting.start) {
         moveRoad();
+        moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
         }
@@ -85,4 +101,19 @@ function moveRoad() {
             line.y = -100;
         }
     });
+}
+
+function moveEnemy() {
+    let enemy = document.querySelectorAll('.enemy');
+    enemy.forEach(function (item) {
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -100 * setting.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        }
+    });
+
+
 }
